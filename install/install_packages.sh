05832d83647110ba3510b9f6a9235c3ea4df1e0c
gerdkortemeyer <kortemey@msu.edu>
2014-02-24 13:29:56 -0500

    Install MongoDB

--- install/install_packages.sh
+++ install/install_packages.sh
@@ -1,6 +1,12 @@
+cp mongodb.repo /etc/yum.repos.d
+yum install mongo-10gen mongo-10gen-server
 yum install memcached
 cpan install Safe
+cpan install MongoDB
 cpan install Safe::Hole
 cpan install Math::Cephes
 cpan install Math::Random
 cpan install Cache::Memcached
+cpan install Locale::Maketext
+cpan install Lingua::Bork
+cpan install Lingua::PigLatin
