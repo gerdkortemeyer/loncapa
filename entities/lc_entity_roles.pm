d596d786f5fafe3169e5be6afbc321daa4c03104
gerdkortemeyer <kortemey@msu.edu>
2014-03-11 20:50:13 -0400

    More work on roles

--- entities/lc_entity_roles.pm
+++ entities/lc_entity_roles.pm
@@ -28,7 +28,7 @@ use Apache::lc_json_utils();
 use Apache::lc_postgresql();
 use Apache::lc_memcached();
 use Apache::lc_entity_utils();
-
+use Apache::lc_init_cluster_table();
 use Apache2::Const qw(:common :http);
 
 #
@@ -36,13 +36,14 @@ use Apache2::Const qw(:common :http);
 # This would also be the routine that's called remotely
 #
 sub local_modify_rolerecord {
-   my ($entity,$domain,$role)=@_;
+   my ($entity,$domain,$rolerecord)=@_;
 # Better make sure before we make a mess
    unless (&Apache::lc_entity_utils::we_are_homeserver($entity,$domain)) {
+      &logwarning("Cannot store rolerecord for ($entity) ($domain), not homeserver");
       return undef;
    }
 # Okay, store
-   &Apache::lc_mongodb::update_roles($entity,$domain,$role);
+   &Apache::lc_mongodb::update_roles($entity,$domain,$rolerecord);
    return 1;
 }
 
@@ -51,9 +52,9 @@ sub local_modify_rolerecord {
 # Send to a *** particular *** other server
 #
 sub remote_modify_rolerecord {
-   my ($host,$entity,$domain,$role)=@_;
+   my ($host,$entity,$domain,$rolerecord)=@_;
    my ($code,$response)=&Apache::lc_dispatcher::command_dispatch($host,'modify_rolerecord',
-                           &Apache::lc_json_utils::perl_to_json({ entity => $entity, domain => $domain, role => $role }));
+                           &Apache::lc_json_utils::perl_to_json({ entity => $entity, domain => $domain, rolerecord => $rolerecord }));
    if ($code eq HTTP_OK) {
       return 1;
    } else {
@@ -61,7 +62,6 @@ sub remote_modify_rolerecord {
    }
 
 }
-
 
 #
 # We are the server that hosts the entity for which a role is given out
@@ -74,6 +74,27 @@ sub local_modify_rolelist {
        $role,
        $startdate,$enddate,
        $manualenrollentity,$manualenrolldomain)=@_;
+# First make sure this is really for us
+  if ($roleentity) {
+# This is a regular role, make sure that we are homeserver
+      unless (&Apache::lc_entity_utils::we_are_homeserver($roleentity,$roledomain)) {
+         &logwarning("Cannot store rolelist for ($roleentity) ($roledomain), not homeserver");
+         return undef;
+      }
+   } else {
+# This is a domain or system role. We better be cluster manager!
+      unless (&Apache::lc_init_cluster_table::we_are_manager()) {
+         &logwarning("Cannot store rolelist, not cluster_manager");
+         return undef;
+      }
+   }
+# Okay, we are in charge here
+   &Apache::lc_postgresql::modify_rolelist($roleentity,$roledomain,$rolesection,
+                                           $userentity,$userdomain, 
+                                           $role, 
+                                           $startdate,$enddate,
+                                           $manualenrollentity,$manualenrolldomain);
+   return 1;
 }
 
 #
@@ -89,6 +110,18 @@ sub remote_modify_rolelist {
        $role,
        $startdate,$enddate,
        $manualenrollentity,$manualenrolldomain)=@_;
+   my ($code,$response)=&Apache::lc_dispatcher::command_dispatch($host,'modify_rolelist',
+"{  roleentity:'$roleentity',roledomain:'$roledomain',rolesection:'$rolesection',
+   userentity:'$userentity',usersdomain:'$userdomain',
+   role:'$role',
+   startdate:'$startdate',enddate:'$enddate',
+   manualenrollentity:'$manualenrollentity',manualenrolldomain:'$manualenrolldomain' 
+}");
+   if ($code eq 'HTTP_OK') {
+      return 1;
+   } else {
+      return undef;
+   }
 }
 
 #
@@ -100,11 +133,86 @@ sub remote_modify_rolelist {
 #
 sub modify_role {
    my ($entity,$domain, # who gets the role?
+       $type, # system, domain, course, user
        $roleentity,$roledomain,$rolesection, # what's the realm?
        $role, # what role is this?
        $startdate,$enddate, # duration
        $manualenrollentity,$manualenrolldomain # if done manually, who did this?
       )=@_;
+# === Deal with the user's record
+# Role itself
+   my $thisrole;
+   $thisrole->{'role'}=$role;
+   $thisrole->{'startdate'}=$startdate;
+   $thisrole->{'enddate'}=$enddate;
+   $thisrole->{'manualenrollentity'}=$manualenrollentity;
+   $thisrole->{'manualenrolldomain'}=$manualenrolldomain;
+# Assemble the role record
+   my $rolerecord;
+# Different types of roles have different structures
+   if ($type eq 'system') {
+# System-level role
+      $rolerecord->{'system'}=$thisrole;
+   } elsif ($type eq 'domain') {
+# Domain-level role
+      $rolerecord->{'domain'}->{$roledomain}=$thisrole;
+   } elsif ($type eq 'course') {
+# Course-level role
+# Do we have a section?
+      if ($rolesection) {
+         $rolerecord->{'course'}->{$roledomain}->{$roleentity}->{'section'}->{$rolesection}=$thisrole;
+      } else {
+         $rolerecord->{'course'}->{$roledomain}->{$roleentity}->{'any'}=$thisrole;
+      }
+   } elsif ($type eq 'user') {
+# Role for another user
+      $rolerecord->{'user'}->{$roledomain}->{$roleentity}=$thisrole;
+   }
+# Have the complete rolerecord now, which goes to the user
+   if (&Apache::lc_entity_utils::we_are_homeserver($entity,$domain)) {
+      &local_modify_rolerecord($entity,$domain,$rolerecord);
+   } else {
+      &remote_modify_rolerecord(&Apache::lc_entity_utils::homeserver($entity,$domain),$entity,$domain,$rolerecord);
+   }
+# === Deal with the rolelist lookup table according to type
+   if (($type eq 'system') || ($type eq 'domain')) {
+# This needs to go to the cluster manager
+# Maybe that's us?
+      if (&Apache::lc_init_cluster_table::we_are_manager()) {
+         &local_modify_rolelist($roleentity,$roledomain,$rolesection,
+                                $entity,$domain,
+                                $role,
+                                $startdate,$enddate,
+                                $manualenrollentity,$manualenrolldomain);
+      } else {
+# No, we are not the cluster manager
+         &remote_modify_rolelist(&Apache::lc_init_cluster_table::cluster_manager(),
+                                $roleentity,$roledomain,$rolesection,
+                                $entity,$domain,
+                                $role,
+                                $startdate,$enddate,
+                                $manualenrollentity,$manualenrolldomain);
+      }
+   } else {
+# Goes on the homeserver of the roleentity
+# Maybe that's us?
+      if (&Apache::lc_entity_utils::we_are_homeserver($roleentity,$roledomain)) {
+         &local_modify_rolelist($roleentity,$roledomain,$rolesection,
+                                $entity,$domain,
+                                $role,
+                                $startdate,$enddate,
+                                $manualenrollentity,$manualenrolldomain);
+      } else {
+# Nope, not us
+         &remote_modify_rolelist(&Apache::lc_entity_utils::homeserver($roleentity,$roledomain),
+                                $roleentity,$roledomain,$rolesection,
+                                $entity,$domain,
+                                $role,
+                                $startdate,$enddate,
+                                $manualenrollentity,$manualenrolldomain);
+      }
+   }
+   return 1;
 }
 
 BEGIN {
@@ -114,7 +222,7 @@ BEGIN {
                   'role',
                   'startdate','enddate',
                   'manualenrollentity','manualenrolldomain');
-    &Apache::lc_connection_handle::register('modify_rolerecord',undef,undef,undef,\&local_modify_rolerecord,'entity','domain','role');
+    &Apache::lc_connection_handle::register('modify_rolerecord',undef,undef,undef,\&local_modify_rolerecord,'entity','domain','rolerecord');
 }
 
 1;
