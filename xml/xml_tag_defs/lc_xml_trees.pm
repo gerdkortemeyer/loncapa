0a72426ee74683893719a38451a25ba08885e2cf
gerdkortemeyer <kortemey@msu.edu>
2014-04-05 08:55:02 -0400

    Start XML support for jsTree

---
+++ xml/xml_tag_defs/lc_xml_trees.pm
@@ -0,0 +1,30 @@
+# The LearningOnline Network with CAPA - LON-CAPA
+# Implements elements for jsTree
+#
+# Copyright (C) 2014 Michigan State University Board of Trustees
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+package Apache::lc_xml_trees;
+
+use strict;
+
+our @ISA = qw(Exporter);
+
+# Export all tags that this module defines in the list below
+our @EXPORT = qw();
+
+
+1;
+__END__
