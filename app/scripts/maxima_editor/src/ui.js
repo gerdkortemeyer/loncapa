bbcd339dd9f1023601c7f4713a2a64b130c4ed8e
damien <damien-git@users.noreply.github.com>
2014-05-07 10:12:36 -0400

    using the class maxima instead of an id

--- app/scripts/maxima_editor/src/ui.js
+++ app/scripts/maxima_editor/src/ui.js
@@ -1,12 +1,37 @@
+/*
 
-var oldtxt = "";
-var ta = document.getElementById('input');
-var output_div = document.getElementById('output');
-var handleChange = function(e) {
-    var txt, parser, output, root;
+Copyright (C) 2014  Michigan State University Board of Trustees
+
+The JavaScript code in this page is free software: you can
+redistribute it and/or modify it under the terms of the GNU
+General Public License (GNU GPL) as published by the Free Software
+Foundation, either version 3 of the License, or (at your option)
+any later version.  The code is distributed WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+
+As additional permission under GNU GPL version 3 section 7, you
+may distribute non-source (e.g., minimized or compacted) forms of
+that code without the copy of the GNU GPL normally required by
+section 4, provided you include this license notice and a URL
+through which recipients can access the Corresponding Source.
+
+*/
+
+/*
+  This script looks for elements with the "maxima" class, and
+  adds a preview div afterward which is updated automatically.
+*/
+
+var handleChange = function(maxima_object) {
+    // maxima_object has 3 fields: ta, output_div, oldtxt
+    // we need to pass this object instead of the values because oldtxt will change
+    var ta, output_div, txt, parser, output, root;
+    ta = maxima_object.ta;
+    output_div = maxima_object.output_div;
     txt = ta.value;
-    if (txt != oldtxt) {
-        oldtxt = txt;
+    if (txt != maxima_object.oldtxt) {
+        maxima_object.oldtxt = txt;
         while (output_div.firstChild != null)
             output_div.removeChild(output_div.firstChild);
         output_div.removeAttribute("title");
@@ -19,7 +44,7 @@ var handleChange = function(e) {
                     math.setAttribute("display", "block");
                     math.appendChild(root.toMathML());
                     output_div.appendChild(math);
-                    MathJax.Hub.Queue(["Typeset", MathJax.Hub, "output"]);
+                    MathJax.Hub.Queue(["Typeset", MathJax.Hub, output_div]);
                 }
             } catch (e) {
                 output = "error: " + e;
@@ -41,6 +66,32 @@ var handleChange = function(e) {
         }
     }
 }
-ta.addEventListener('change', handleChange, false);
-ta.addEventListener('keyup', handleChange, false);
-window.addEventListener('load', function(e) { ta.focus(); handleChange(e); }, false);
+
+window.addEventListener('load', function(e) {
+    var maxima_objects = [];
+    var maxima_inputs = document.getElementsByClassName('maxima');
+    for (var i=0; i<maxima_inputs.length; i++) {
+        var ta = maxima_inputs[i];
+        var output_div = document.createElement("div");
+        if (ta.nextSibling)
+            ta.parentNode.insertBefore(output_div, ta.nextSibling);
+        else
+            ta.parentNode.appendChild(output_div);
+        var oldtxt = "";
+        maxima_objects[i] = {
+            "ta": ta,
+            "output_div": output_div,
+            "oldtxt": oldtxt
+        };
+        var changeObjectN = function(n) {
+            return function(e) { handleChange(maxima_objects[n]); };
+        }
+        var startChange = changeObjectN(i);
+        if (ta.value != oldtxt)
+            startChange();
+        ta.addEventListener('change', startChange , false);
+        ta.addEventListener('keyup', startChange , false);
+    }
+    
+}, false);
+
