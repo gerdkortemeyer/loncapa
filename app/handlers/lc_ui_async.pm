48c89ca8681fcf500a921579092f3964b5034afd
gerdkortemeyer <kortemey@msu.edu>
2014-06-27 09:57:55 -0400

    Structure for asynchronous jobs

--- app/handlers/lc_ui_async.pm
+++ app/handlers/lc_ui_async.pm
@@ -26,21 +26,49 @@ use Apache::lc_entity_sessions();
 
 use Apache::lc_logs;
 
+my $job;
+
 # ==== Main handler
+# Just takes the job, remembers it, and says "ok"
+# The actual work is done asynchronously
 #
 sub handler {
 # Get request object
    my $r = shift;
    $r->content_type('application/json; charset=utf-8');
    my %content=&Apache::lc_entity_sessions::posted_content();
-$r->print("ok");
+# The job must include the command and all needed parameters
+   $job=undef;
+   foreach my $key (keys(%content)) {
+      $job->{$key}=$content{$key};
+   }
+# Did we get anything?
+   if ($job->{'command'}) {
+      $r->print("ok");
+   } else {
+      $r->print("error");
+   }
    return OK;
 }
 
+# === The commands
+#
+sub usersearch {
+   my ($term)=@_;
+   &logdebug("Got search term $term");
+}
+
+#
 # ==== The actual business logic
+# This gets called as a cleanup action, so it will run in the background
 #
 sub main_actions {
-   &logdebug("Main action");
+# Pick up the job ticket and see if we can do it
+   if ($job->{'command'} eq 'usersearch') {
+      &usersearch($job->{'term'});
+   } else {
+      &logwarning("Unknown asynchronous job command: [".$job->{'command'}."]");
+   }
 }
 
 
