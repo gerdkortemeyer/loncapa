4dde2c5ac6244a7eb4ebb152d4e5d75d484d43b4
gerdkortemeyer <kortemey@msu.edu>
2014-07-02 13:29:10 -0400

    Go to next stage of manually adding users

--- app/scripts/lc_add_to_courselist.js
+++ app/scripts/lc_add_to_courselist.js
@@ -1,2 +1,15 @@
+$(document).ready(function() {
+    $('#continue').click(function() {
+        modify_selected();
+    });
+} );
 
-
+function modify_selected() {
+   if ($('#user_username').val().length>0) {
+      document.searchnewuser.method="post";
+      document.searchnewuser.action="/pages/lc_modify_courselist.html";
+      parent.setbreadcrumbbar('add','modifycourselist','Modify Selected Entries','');
+      parent.breadcrumbbar();
+      document.searchnewuser.submit();
+   }
+}
