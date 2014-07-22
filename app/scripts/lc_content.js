b34681ce4192b6e8b66c613b7b86589137e25470
gerdkortemeyer <kortemey@msu.edu>
2014-07-22 17:05:57 -0400

    Trying to initialize jsTree

--- app/scripts/lc_content.js
+++ app/scripts/lc_content.js
@@ -1,2 +1,11 @@
-
+$(document).ready(function() {
+   $('#content_tree').jstree({ 'core' : {
+    'data' : [
+       { "id" : "ajson1", "parent" : "#", "text" : "Simple root node" },
+       { "id" : "ajson2", "parent" : "#", "text" : "Root node 2" },
+       { "id" : "ajson3", "parent" : "ajson2", "text" : "Child 1" },
+       { "id" : "ajson4", "parent" : "ajson2", "text" : "Child 2" },
+    ]
+} });
+});
 
