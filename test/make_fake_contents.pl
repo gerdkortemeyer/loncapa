41d300f7d07f3dc7c5b178382d35fdc4145308de
gerdkortemeyer <kortemey@msu.edu>
2014-07-24 11:11:47 -0400

    Start work on making large fake tables of content

---
+++ test/make_fake_contents.pl
@@ -0,0 +1,67 @@
+use strict;
+use Data::Uniqid qw(luniqid);
+use JSON::DWIW;
+
+
+my $toc;
+
+&make_folder($toc);
+
+print &perl_to_json($toc);
+exit;
+
+sub make_folder {
+   my ($current_toc)=@_;
+   for (my $i=0;$i<=int(rand(20));$i++) {
+      if (int(rand()+0.6)) {
+print "Res\n";
+         push(@{$current_toc},&new_asset(&make_unique_id(),('msu','sfu','ostfalia')[int(rand(2)+0.5)],&make_fake_title()));
+      } else {
+print "Folder\n";
+         push(@{$current_toc},&new_folder('Chapter '.&make_fake_title()));
+         &make_folder($current_toc->[-1]->{'content'});
+      }
+   }
+   print &perl_to_json($current_toc)."\n======\n";
+}
+
+
+sub perl_to_json {
+   return JSON::DWIW->new->to_json(@_[0],{ pretty => 1 });
+}
+
+
+sub make_fake_title {
+   my @title=();
+   for (my $i=0; $i<=2+int(rand(2)); $i++) {
+      push(@title,('capacity','electronic','current','resistance','field','energy','fusion')[int(rand(6)+0.5)]);
+   }
+   return join(' ',@title);
+}
+
+sub new_asset {
+   my ($resentity,$resdomain,$restitle)=@_;
+   return { entity => $resentity, domain => $resdomain, 
+            title => $restitle, 
+            type => 'asset', 
+            active => 1, hidden => 0, 
+            id => &long_unique_id() }
+}
+
+sub new_folder {
+   my ($foldertitle)=@_;
+   return { title => $foldertitle, type => 'folder',
+            active => 1, hidden => 0, 
+            id => &long_unique_id(), 
+            content => [] } 
+}
+
+
+sub make_unique_id {
+   return &luniqid();
+}
+
+sub long_unique_id {
+   return &make_unique_id().'_'.$$.'_'.time;
+}
+
