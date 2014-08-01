69edcb54d75c09aeb8f00a9a80ef1fe264a205d3
damien <damien-git@users.noreply.github.com>
2014-08-01 13:30:15 -0400

    improved tidy cleaning with files using sty by looking at actual tag usage, and made clean.bash callable from another directory

--- conversion/clean.bash
+++ conversion/clean.bash
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# link resolution - $0 can be a symbolic link
+PRG="$0"
+progname=`basename "$0"`
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`"/$link"
+  fi
+done
+MY_HOME=`dirname "$PRG"`
+# absolute path
+MY_HOME=`cd "$MY_HOME" && pwd`
+
+# create a name for the clean file
 pathname="$1"
 dir=$(dirname "$pathname")
 filename=$(basename "$pathname")
@@ -11,6 +28,11 @@ fi
 namenoext="${filename%.*}"
 newpath="$dir/${namenoext}_clean$ext"
 
-perl pre_tidy.pl "$pathname" >/tmp/pretidy.txt
-tidy -config /tmp/tidycfg.txt /tmp/pretidy.txt | perl post_tidy.pl > "$newpath"
+
+perl $MY_HOME/pre_tidy.pl "$pathname" >/tmp/pretidy.txt
+if [ $? -ne 0 ]; then
+  exit
+fi
+# warning: using constant temp file names here prevents running several instances at the same time
+tidy -config /tmp/tidycfg.txt /tmp/pretidy.txt | perl $MY_HOME/post_tidy.pl > "$newpath"
 
