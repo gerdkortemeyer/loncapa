6c6faa55aa46c489911399cb46a67c812439c212
gerdkortemeyer <kortemey@msu.edu>
2014-08-04 08:16:58 -0400

    No forward or backward arrows at the ends

--- app/scripts/lc_default.js
+++ app/scripts/lc_default.js
@@ -61,6 +61,7 @@ function display_course_asset(assetid) {
                  });
               } else {
                  $('#navrightlink').click(function(){});
+                 $('#navrightlink').html('');
               }
            }
            if (key=='prev') {
@@ -71,6 +72,7 @@ function display_course_asset(assetid) {
                  });
               } else {
                  $('#navleftlink').click(function(){});
+                 $('#navleftlink').html('');
               }
            }
        });
