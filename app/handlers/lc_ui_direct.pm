c71daab7a9aae0f309e2c4f1b51084e9ecd7256d
gerdkortemeyer <kortemey@msu.edu>
2014-09-12 11:05:37 -0400

    Work on deep-linking

---
+++ app/handlers/lc_ui_direct.pm
@@ -0,0 +1,35 @@
+# The LearningOnline Network with CAPA - LON-CAPA
+# Direct jump to a resource
+#
+# Copyright (C) 2014 Michigan State University Board of Trustees
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+package Apache::lc_ui_direct;
+
+use strict;
+use Apache2::RequestRec();
+use Apache2::Const qw(:common :http);
+use Apache::lc_entity_sessions();
+
+# ==== Main handler
+#
+sub handler {
+# Get request object
+   my $r = shift;
+   $r->headers_out->set('Location' => '/');
+   return REDIRECT;
+}
+1;
+__END__
