e02b90342ea3cfc7d7eca97df7eeeed9848bf538
damien-git <>
2014-10-24 16:30:22 -0400

    added a script to validate a file or directory with libxml2, and changed a regexp in the schema so that libxml recognizes it

---
+++ conversion/validate_clean.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl
+
+# Validates a file or directory against loncapa.xsd with libxml2
+
+use strict;
+use utf8;
+use warnings;
+
+use File::Basename;
+use Try::Tiny;
+use XML::LibXML;
+
+
+binmode(STDOUT, ':encoding(UTF-8)');
+binmode(STDERR, ':encoding(UTF-8)');
+
+if (scalar(@ARGV) != 1) {
+  print STDERR "Usage: perl validate_clean.pl file|directory\n";
+  exit(1);
+}
+
+# find the command-line argument encoding
+use I18N::Langinfo qw(langinfo CODESET);
+my $codeset = langinfo(CODESET);
+use Encode qw(decode);
+@ARGV = map { decode $codeset, $_ } @ARGV;
+
+my $pathname = "$ARGV[0]";
+
+my $script_dir = dirname(__FILE__);
+my $xmlschema = XML::LibXML::Schema->new(location => $script_dir.'/loncapa.xsd');
+
+if (-d "$pathname") {
+  validate_dir($pathname);
+} elsif (-f $pathname) {
+  validate_file($pathname);
+}
+
+
+# Validates a directory recursively, selecting only _clean.problem files.
+sub validate_dir {
+  my ($dirpath) = @_;
+  
+  opendir (my $dh, $dirpath) or die $!;
+  while (my $entry = readdir($dh)) {
+    next if ($entry =~ m/^\./); # ignore entries starting with a period
+    my $pathname = $dirpath.'/'.$entry;
+    if (-d $pathname) {
+      validate_dir($pathname);
+    } elsif (-f $pathname) {
+      if ($pathname =~ /_clean\.problem$/) {
+        validate_file($pathname);
+      }
+    }
+  }
+  closedir($dh);
+}
+
+# Validates a file against loncapa.xsd with libxml2
+sub validate_file {
+  my ($pathname) = @_;
+  
+  my $doc = XML::LibXML->load_xml(location => $pathname);
+  try {
+    $xmlschema->validate($doc);
+    print "$pathname is valid\n";
+  } catch {
+    print "$_\n";
+  }
+}
