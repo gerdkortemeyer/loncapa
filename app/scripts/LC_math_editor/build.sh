657c8e06cb7a21a7d8934e997fb49cb588ee7c58
damien <damien-git@users.noreply.github.com>
2014-12-03 10:57:33 -0500

    using a local closure compiler whenever possible (the web service does not seem to work anymore)

--- app/scripts/LC_math_editor/build.sh
+++ app/scripts/LC_math_editor/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# this builds dist/LC_math_editor.min.js using google closure compiler with compressjs.
+# this builds dist/LC_math_editor.min.js using google closure compiler.
 # It must be run in the math_editor directory.
 
 cd "$(dirname "$0")"
@@ -23,6 +23,13 @@ cat <<% >>$tmp
 }();
 %
 
+# use the local application if Java is available
+if hash java 2>/dev/null; then
+    java -jar lib/compiler.jar --language_in=ECMASCRIPT5_STRICT --compilation_level=SIMPLE_OPTIMIZATIONS $tmp >dist/LC_math_editor.min.js
+    exit 0
+fi
+
+# otherwise use the web service with compressjs.sh
 NEWFILE="c`date +"%d%m%y"`.js"
 /bin/sh ./compressjs.sh -strict $tmp
 mv "$NEWFILE" ./dist/LC_math_editor.min.js
