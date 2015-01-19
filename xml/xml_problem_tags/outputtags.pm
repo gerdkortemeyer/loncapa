d681eba75839b111c483e2eedc6489833d8f8dff
Corey <musolffc@gmail.com>
2015-01-19 08:25:42 -0500

    Working on <num> formatting tag

--- xml/xml_problem_tags/outputtags.pm
+++ xml/xml_problem_tags/outputtags.pm
@@ -21,7 +21,6 @@ package Apache::xml_problem_tags::outputtags;
 use strict;
 use Apache::lc_asset_safeeval();
 use Math::SigFigs;
-use Number::Format;
 
 use Apache::lc_logs;
 
@@ -45,28 +44,78 @@ sub format_sigfigs {
 # Using Number::Format
 # Format like '3e' 
 #
-sub format_scientific {
-   my ($num,$digits)=@_;
-   return $num;
-}
+#sub format_scientific {
+#   my ($num,$digits)=@_;
+#   return sprintf("%.$digits"."e", $num);
+#}
 
 #
 # Format a number according to a formatting string, e.g., "3s"
 # Also exported to safespace
 #
-sub format {
-   my ($num,$formatstring)=@_;
-   if ($formatstring=~/^(\d+)s$/is) {
-      return &format_sigfigs($num,$1);
-   } elsif ($formatstring=~/^(\d+)e$/is) {
-      return &format_scientific($num,$1);
-   } else {
-# No idea what the format is supposed to be, just return
-      return $num;
-   }
-}
+#sub format {
+#   my ($num,$formatstring)=@_;
+#   if ($formatstring=~/^(\d+)s$/is) {
+#      return &format_sigfigs($num,$1);
+#   } elsif ($formatstring=~/^(\d+)e$/is) {
+#      return &format_scientific($num,$1);
+#   } else {
+## No idea what the format is supposed to be, just return
+#      return $num;
+#   }
+#}
  
 
+
+
+sub format {
+    my ($value,$fmt)=@_;
+    my $result;
+    #if ($fmt =~ /chem/i) { return(&chemparse($value)); }
+    my ($dollarmode,$commamode,$alwaysperiod,$options);
+    if ($fmt =~ /^([^\d]*)(.*)/) { $options=$1; $fmt=$2; }
+    if ($options =~ /\$/) { $dollarmode=1; }
+    if ($options =~ /,/)  { $commamode=1; }
+    if ($options =~ /\./) { $alwaysperiod=1; }
+    if ($fmt=~/^(\d+)s$/is) {
+       $value=&format_sigfigs($value,$1);
+    } elsif ($fmt) {
+       $value=sprintf('%.'.$fmt,$value);
+    }
+    if ($alwaysperiod && $fmt eq '0f') {
+       #if ($target eq 'tex') {
+       #   $value .='\\ensuremath{.}';
+       #} else {
+          $value .='.';
+       #}
+    }
+    if ($value =~ /([0-9\.\-\+]+)[eE]([0-9\-\+]+)/i ) {
+       my $frac=$1;
+       if ($dollarmode) { $frac=&dollarformat($frac); }
+       if ($commamode) { $frac=&commaformat($frac); }
+       my $exponent=$2;
+       $exponent=~s/^\+0*//;
+       $exponent=~s/^-0*/-/;
+       $exponent=~s/^-0*/-/;
+       if ($exponent eq '-') { undef($exponent); }
+       if ($exponent) {
+          #if ($target eq 'web') {
+             $result=$frac.'&#215;10<sup>'.$exponent.'</sup>';
+          #} elsif ($target eq 'tex') {
+          #   $result='\ensuremath{'.$frac.'\times 10^{'.$exponent.'}}';
+          #} else {
+          #   $result=$value;
+          #}
+       } else {
+          $result=$frac;
+       }
+    } else {
+       $result=$value;
+       #if    ($dollarmode) { $result=&dollarformat($result,$target); }
+       #elsif ($commamode)  { $result=&commaformat($result,$target); }
+    }
+    return $result;
+}
 
 #
 sub start_num_html {
@@ -76,6 +125,7 @@ sub start_num_html {
    $p->get_token;
    pop(@{$stack->{'tags'}});
 # Evaluate all variables that may be in there inside safespace, return formatted version
+   #return &format(&Apache::lc_asset_safeeval::texteval($safe,$text),$token->[2]->{'format'});
    return &format(&Apache::lc_asset_safeeval::texteval($safe,$text),$token->[2]->{'format'});
 }
 
