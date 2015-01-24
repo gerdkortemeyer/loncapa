e92e4f57b0ab8b20b8bf3494a6514d905cb9a9f2
gerdkortemeyer <kortemey@msu.edu>
2015-01-24 18:43:35 -0500

    Work on external call conversion

--- conversion/ext/ext_convert.pl
+++ conversion/ext/ext_convert.pl
@@ -5,20 +5,129 @@ use strict;
 #
 sub ext_to_func {
    my ($ext)=@_;
-   unless ($ext=~/^\s*\&EXT\(/) { return $ext; }
+   my $error="'*** ERROR ***'";
+   unless ($ext=~/^\s*\&EXT\(/) { return $error; }
+   if ($ext=~/\,/) { return $error; }
    $ext=~s/^\s*\&EXT\s*\(\s*//;
    $ext=~s/\s*\)\s*$//s;
 # Get rid of Perlisms
    $ext=~s/\'//gs;
    $ext=~s/\"//gs;
    $ext=~s/\.+/\./gs;
+   $error.=' ('.$ext.')';
 # Now argument looks like user.resource.resource.$partid.$responseid.submission
-   print $ext."\n";
+   my @comp=split(/\./,$ext);
+   if ($comp[0] eq 'environment') {
+      if ($comp[1] eq 'firstname') {
+         return '&firstname()';
+      } elsif ($comp[1] eq 'middlename') {
+         return '&middlename()';
+      } elsif ($comp[1] eq 'lastname') {
+         return '&lastname()';
+      } elsif ($comp[1] eq 'generation') {
+         return '&suffix()';
+      } elsif ($comp[1] eq 'id') {
+         return '&student_number()';
+      } elsif ($comp[1] eq 'nickname') {
+         return '&nickname()';
+      } elsif ($comp[1] eq 'screenname') {
+         return '&screenname()';
+      } elsif ($comp[1] eq 'picture') {
+         return '0';
+      } elsif ($comp[1] eq 'permanentemail') {
+         return '&permanent_email()';
+      } elsif ($comp[1] eq 'critnotification') {
+         return '0';
+      } elsif ($comp[1] eq 'notification') {
+         return '0';
+      } else {
+         return $error;
+      }
+   } elsif ($comp[0] eq 'user') {
+      return '';
+   } elsif ($comp[0] eq 'query') {
+      return '';
+   } elsif ($comp[0] eq 'request') {
+      if ($comp[1] eq 'filename') {
+         return '&request_filename()';
+      } elsif ($comp[1] eq 'uri') {
+         return '&request_uri()';
+      } elsif ($comp[1] eq 'state') {
+         return '&request_state()';
+      } elsif ($comp[1] eq 'browser') {
+         if ($comp[2] eq 'os') {
+            return '&browser_os()';
+         } elsif ($comp[2] eq 'unicode') {
+            return '1';
+         } elsif ($comp[2] eq 'mathml') {
+            return '0';
+         } elsif ($comp[2] eq 'textremote') {
+            return '0';
+         } elsif ($comp[2] eq 'type') {
+            return '&browser_type()';
+         } elsif ($comp[2] eq 'version') {
+            return '&browser_version()';
+         } else {
+            return $error;
+         }
+      } elsif ($comp[1] eq 'host') {
+         return '&request_host()';
+      } elsif ($comp[1] eq 'role') {
+         return '&request_role()';
+      } elsif (($comp[1] eq 'course') && ($comp[2] eq 'id')) {
+         return '&classid()';
+      } elsif (($comp[1] eq 'course') && ($comp[2] eq 'sec')) {
+         return '&sec()';
+      } else {
+         return $error;
+      }
+   } elsif ($comp[0] eq 'course') {
+      if (($comp[1] eq 'description') || ($comp[1] eq 'decription')) {
+         return '&class()';
+      } elsif ($comp[1] eq 'domain') {
+         return '&classdomain()';
+      } elsif ($comp[1] eq 'num') {
+         return '&classentity()';
+      } elsif ($comp[1] eq 'url') {
+         return '&classcode()';
+      } elsif ($comp[1] eq 'id()') {
+         return '&classid()';
+      } else {
+         return $error;
+      }
+   } elsif ($comp[0] eq 'resource') {
+      if ($comp[-1] eq 'tolerance') {
+      } elsif ($comp[-1] eq 'maxtries') {
+      } elsif ($comp[-1] eq 'weight') {
+      } elsif ($comp[-1] eq 'duedate') {
+      } elsif ($comp[-1] eq 'answerdate') {
+      } elsif ($comp[-1] eq 'opendate') {
+      } elsif ($comp[-1] eq 'problemstatus') {
+      } elsif ($comp[-1] eq 'scoreformat') {
+      } elsif ($comp[-1] eq 'title') {
+      } elsif ($comp[-1] eq 'subject') {
+      } elsif ($comp[-1] eq 'keywords') {
+      } else {
+         return $error;
+      }
+   } elsif ($comp[0] eq 'system') {
+      return '';
+   } elsif ($comp[0]=~/^parameter/) {
+      my (undef,$partid,$which)=split(/\_/,$comp[0]);
+      if ($partid) {
+         return '&parameter_setting("'.$which.'"."'.$partid.'")';
+      } else {
+         return '&parameter_setting("'.$which.'")';
+      }
+   } else {
+      return $error;
+   }
 }
 
 open(IN,"ext_calls.txt");
 while (my $line=<IN>) {
-   &ext_to_func($line);
+   chomp($line);
+   print $line." => ".&ext_to_func($line)."\n";
 }
 close(IN);
 
